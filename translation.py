b486f72ee651b7354e2c89bd4b44bd604b2d937e
Dasun Pamod <63133683+dasunpamod@users.noreply.github.com>
2021-02-10 01:16:21 -0800

    Update translation.py

--- translation.py
+++ translation.py
@@ -8,7 +8,7 @@ This is a Telegram URL Upload Bot!
 
 /help for more details..
 
-
+ """
 
     HELP_USER = """Hai I'am a URL Uploader bot..
     
@@ -20,7 +20,7 @@ This is a Telegram URL Upload Bot!
    Video  - Give File as video without Screenshots
    DFile  - Give File without Screenshots
 
-
+ """
 
     FORMAT_SELECTION = """Select the desired format: <a href='{}'>file size might be approximate</a>
     
